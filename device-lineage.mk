cf89b8eeba87613fa044384af7e3a75785a01c24
Nolen Johnson <johnsonnolen@gmail.com>
2018-02-05 08:05:00 -0600

    TP1803L: Enable camera api v2 in Snap
    Change-Id: Iac94494fd1c59b8381f79c3d6dd981cca0a74175

--- device-lineage.mk
+++ device-lineage.mk
@@ -6,6 +6,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
