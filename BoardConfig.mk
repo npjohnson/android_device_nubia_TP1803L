cb49bb1b9bf84d3d37a29a33817a987db259dd9c
P.Adarsh Reddy <padarshr@codeaurora.org>
2020-03-09 12:16:37 +0530

    TP803L: Add build broken flags in BoardConfig.mk
    BUILD_BROKEN_USES_BUILD_COPY_HEADERS
    
    Change-Id: I7b115f5bb7990c438c595b4b1ff536598786d529

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,8 @@
 include device/nubia/TP1803/BoardConfig.mk
 
+# Headers
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+
 # Manifests
 DEVICE_MANIFEST_FILE += device/nubia/TP1803L/lineage_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/nubia/TP1803L/lineage_compatibility_matrix.xml
