bf196fa3932653a1c8837c16f354e0a32c3dc110
LuK1337 <priv.luk@gmail.com>
2020-07-01 17:36:26 +0200

    TP1803L: Include dtbo in recovery image
    * This allows us to drop dependency on dtbo partition
      being in sync with recovery.
    * Test: fastboot erase dtbo && fastboot boot recovery.img
    
    Change-Id: Id0b36db9634ba7b20143c247c68da5d31891fce1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,7 @@
 include device/nubia/TP1803/BoardConfig.mk
 
+# DTBO
+BOARD_INCLUDE_RECOVERY_DTBO := true
+
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/nubia/TP1803L/lineage_compatibility_matrix.xml
